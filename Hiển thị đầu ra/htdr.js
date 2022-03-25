f1a43356119c95f27874d6536d5e98d49a6acf64
Nguyen-Duy-Phat <102140023+Nguyen-Duy-Phat@users.noreply.github.com>
2022-03-25 17:08:19 +0700

    done

---
+++ Hiển thị đầu ra/htdr.js
@@ -0,0 +1,12 @@
+// function show(){
+//     alert("Nguyễn Vũ Thiên Di");
+// }
+// function showdiv(){
+//     document.getElementById("showdiv").innerHTML = "Nguyễn Vũ Thiên Di";
+// }
+// function show(){
+//     document.write("Nguyễn Vũ Thiên Di");
+// }
+// function show(){
+//     console.log("Nguyễn Vũ Thiên Di");
+// }
---
+++ JS Lý thuyết/demojs.js
@@ -0,0 +1,6 @@
+// function showM(){
+//     alert('Xin Chào bạn');
+// }
+// function showM(){
+//     document.write('Xìn chào nhá')
+// }
